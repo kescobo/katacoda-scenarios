5bd932e4c0d07e85d4833124d5fa3f206bb8ef3b
Marielle <marielle@widged.com>
2020-10-18 20:38:32 +0200

    install julia with wget

--- julia-repl/set-env.sh
+++ julia-repl/set-env.sh
@@ -1,3 +1,4 @@
 # do some magic
-sudo apt-get update -y
-sudo apt-get install -y julia
+wget https://julialang-s3.julialang.org/bin/linux/x64/1.5/julia-1.5.2-linux-x86_64.tar.gz
+tar xf julia-1.3.0-linux-x86_64.tar.gz
+sudo ln -s ~/julia-1.3.0/bin/julia /usr/local/bin/julia
