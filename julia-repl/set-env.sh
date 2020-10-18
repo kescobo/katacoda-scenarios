7f2031d8134babc11eda8e544c6ff99a58015093
Marielle <marielle@widged.com>
2020-10-18 20:49:00 +0200

    clear terminal

--- julia-repl/set-env.sh
+++ julia-repl/set-env.sh
@@ -2,3 +2,4 @@
 wget https://julialang-s3.julialang.org/bin/linux/x64/1.5/julia-1.5.2-linux-x86_64.tar.gz
 tar xf julia-1.5.2-linux-x86_64.tar.gz
 sudo ln -s ~/julia-1.5.2/bin/julia /usr/local/bin/julia
+clear
